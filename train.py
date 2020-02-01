21ddcecab194584bc5b0e4b047f0de975f832af6
WillHua127 <48099255+WillHua127@users.noreply.github.com>
2020-01-31 22:06:39 -0500

    Add files via upload

--- train.py
+++ train.py
@@ -12,6 +12,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from torch.autograd import Variable
+from sklearn import metrics
 
 from utils import load_data
 from models import GAT, SpGAT
@@ -24,13 +25,13 @@ parser.add_argument('--fastmode', action='store_true', default=False, help='Vali
 parser.add_argument('--sparse', action='store_true', default=False, help='GAT with sparse version or not.')
 parser.add_argument('--seed', type=int, default=72, help='Random seed.')
 parser.add_argument('--epochs', type=int, default=10000, help='Number of epochs to train.')
-parser.add_argument('--lr', type=float, default=0.005, help='Initial learning rate.')
-parser.add_argument('--weight_decay', type=float, default=5e-4, help='Weight decay (L2 loss on parameters).')
-parser.add_argument('--hidden', type=int, default=8, help='Number of hidden units.')
-parser.add_argument('--nb_heads', type=int, default=8, help='Number of head attentions.')
-parser.add_argument('--dropout', type=float, default=0.6, help='Dropout rate (1 - keep probability).')
-parser.add_argument('--alpha', type=float, default=0.2, help='Alpha for the leaky_relu.')
-parser.add_argument('--patience', type=int, default=100, help='Patience')
+parser.add_argument('--lr', type=float, default=0.007, help='Initial learning rate.')
+parser.add_argument('--weight_decay', type=float, default=6e-4, help='Weight decay (L2 loss on parameters).')
+parser.add_argument('--hidden', type=int, default=16, help='Number of hidden units.')
+parser.add_argument('--nb_heads', type=int, default=10, help='Number of head attentions.')
+parser.add_argument('--dropout', type=float, default=0.7, help='Dropout rate (1 - keep probability).')
+parser.add_argument('--alpha', type=float, default=0.3, help='Alpha for the leaky_relu.')
+parser.add_argument('--patience', type=int, default=50, help='Patience')
 
 args = parser.parse_args()
 args.cuda = not args.no_cuda and torch.cuda.is_available()
