a358a19220c51a7760b8110efe73e09c245075f3
WillHua127 <48099255+WillHua127@users.noreply.github.com>
2020-02-01 16:07:19 -0500

    Update utils.py

--- utils.py
+++ utils.py
@@ -16,19 +16,39 @@ def parse_index_file(filename):
         index.append(int(line.strip()))
     return index
 
+def load_dataset(dataset):
+    dense_adj, features, labels = torch.load("%s_dense_adj.pt" % dataset), torch.load("%s_features.pt" % dataset), torch.load("%s_labels.pt" % dataset)
+    indices = torch.nonzero(dense_adj).t(); values = dense_adj[indices[0], indices[1]]
+    adj = torch.sparse.FloatTensor(indices, values, dense_adj.size()).clone()
+    del dense_adj, indices, values
+    
+    if dataset == 'cora':
+        idx_train, idx_val, idx_test = range(140), range(140, 640), range(1708, 2708)
+    elif dataset == 'citeseer':
+        idx_train, idx_val, idx_test = range(120), range(120, 620), range(2312, 3312)
+    elif dataset == 'pubmed':
+        idx_train, idx_val, idx_test = range(60), range(60, 560), range(18717, 19717)
+    
+    idx_train = torch.LongTensor(idx_train)
+    idx_val = torch.LongTensor(idx_val)
+    idx_test = torch.LongTensor(idx_test)
+    
+    return adj, features, labels, idx_train, idx_val, idx_test
+
+
 def load_data(prefix):
     print('Loading {} dataset...'.format(prefix))
     names = ['x', 'y', 'tx', 'ty', 'allx', 'ally', 'graph']
     objects = []
     for i in range(len(names)):
-        with open("./data/{}/ind.{}.{}".format(prefix, prefix, names[i]), 'rb') as f:
+        with open("./dataset/ind.{}.{}".format(prefix, names[i]), 'rb') as f:
             if sys.version_info > (3, 0):
                 objects.append(pkl.load(f, encoding='latin1'))
             else:
                 objects.append(pkl.load(f))
         
     x, y, tx, ty, allx, ally, graph = tuple(objects)
-    test_idx_reorder = parse_index_file("./data/{}/ind.{}.test.index".format(prefix, prefix))   
+    test_idx_reorder = parse_index_file("./dataset/ind.{}.test.index".format(prefix))   
     test_idx_range = np.sort(test_idx_reorder)
 
     if prefix == 'citeseer':
@@ -53,24 +73,14 @@ def load_data(prefix):
     adj = nx.adjacency_matrix(nx.from_dict_of_lists(graph))
     adj = sp.csr_matrix(adj, dtype=np.float32)[:,list(np.where(np.sum(labels,1)==1)[0])][list(np.where(np.sum(labels,1)==1)[0]),:]
     adj = sp.coo_matrix(adj,dtype=np.float32)
-    adj = normalize(adj + sp.eye(adj.shape[0]))
-    
-    s_val = labels[np.random.choice(labels.shape[0], 500, replace=False)]
-    
-    idx_train = range(len(y))
-    idx_val = range(len(y), len(s_val) + 500)
-    idx_test = range(len(s_val) + 500, len(s_val) + 1500)
+    adj = normalize(adj + sp.eye(adj.shape[0])) 
 
     # build symmetric adjacency matrix
     adj = sparse_mx_to_torch_sparse_tensor(adj)
     features = torch.FloatTensor(np.array(features.todense()))
     labels = torch.LongTensor(np.where(labels)[1])
 
-    idx_train = torch.LongTensor(idx_train)
-    idx_val = torch.LongTensor(idx_val)
-    idx_test = torch.LongTensor(idx_test)
-
-    return adj, features, labels, idx_train, idx_val, idx_test
+    return adj, features, labels, 
 
 
 
