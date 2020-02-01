219d0c31a56796405fa80296580c2cb4dfc182eb
WillHua127 <48099255+WillHua127@users.noreply.github.com>
2020-02-01 15:26:31 -0500

    Update models.py

--- models.py
+++ models.py
@@ -22,5 +22,5 @@ class GAT(nn.Module):
         x = F.dropout(x, self.dropout, training=self.training)
         x = torch.cat([att(x, adj) for att in self.attentions], dim=1)
         x = F.dropout(x, self.dropout, training=self.training)
-        x = F.relu6(self.out_att(x, adj))
+        x = F.elu(self.out_att(x, adj))
         return F.log_softmax(x, dim=1)
