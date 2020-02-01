3f009a974dfbf44468da77881eb6cf7e62063313
WillHua127 <48099255+WillHua127@users.noreply.github.com>
2020-02-01 14:36:09 -0500

    Update utils.py

--- utils.py
+++ utils.py
@@ -9,12 +9,6 @@ import networkx as nx
 import math
 import torch 
 
-def encode_onehot(labels):
-    classes = set(labels)
-    classes_dict = {c: np.identity(len(classes))[i, :] for i, c in enumerate(classes)}
-    labels_onehot = np.array(list(map(classes_dict.get, labels)), dtype=np.int32)
-    return labels_onehot
-
 def parse_index_file(filename):
     """Parse index file."""
     index = []
