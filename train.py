6804b995512e0225c7c79855885276ea8cddc5db
WillHua127 <48099255+WillHua127@users.noreply.github.com>
2020-03-01 02:44:57 -0500

    Update train.py

--- train.py
+++ train.py
@@ -24,7 +24,7 @@ parser.add_argument('--dataset', type=str, default='citeseer', help='prefix iden
 parser.add_argument('--no-cuda', action='store_true', default=False, help='Disables CUDA training.')
 parser.add_argument('--fastmode', action='store_true', default=False, help='Validate during training pass.')
 parser.add_argument('--seed', type=int, default=123, help='Random seed.')
-parser.add_argument('--epochs', type=int, default=200000, help='Number of epochs to train.')
+parser.add_argument('--epochs', type=int, default=500000, help='Number of epochs to train.')
 parser.add_argument('--lr', type=float, default=0.001, help='Initial learning rate.')
 parser.add_argument('--weight_decay', type=float, default=6e-4, help='Weight decay (L2 loss on parameters).')
 parser.add_argument('--hidden', type=int, default=16, help='Number of hidden units.')
