dac40d7efa7fa06da58c7ef028f081c176373079
WillHua127 <48099255+WillHua127@users.noreply.github.com>
2020-03-01 02:03:24 -0500

    Update layers.py

--- layers.py
+++ layers.py
@@ -52,7 +52,7 @@ class GraphAttentionLayer(nn.Module):
 
         self.W = nn.Parameter(torch.zeros(size=(in_features, out_features)))
         nn.init.xavier_normal_(self.W.data, gain=1.414)
-        self.a = nn.Parameter(torch.zeros(size=(1, 4*out_features)))
+        self.a = nn.Parameter(torch.zeros(size=(1, 2*out_features)))
         nn.init.xavier_normal_(self.a.data, gain=1.414)
         
         self.dropout = nn.Dropout(dropout)
@@ -75,14 +75,14 @@ class GraphAttentionLayer(nn.Module):
         
 
         h = torch.mm(input, self.W)
-        h = self.relu_bt(h)
+        #h = self.relu_bt(h)
         # h: N x out
         assert not torch.isnan(h).any()
 
         # Self-attention on the nodes - Shared attention mechanism
         agg = self.relu_bt(torch.add(h[edge[0, :], :], h[edge[1, :], :]))       
         diff = self.relu_bt(torch.sub(h[edge[0, :], :], h[edge[1, :], :]))
-        edge_h = torch.cat([h[edge[0, :], :], h[edge[1, :], :], agg, diff], dim=1).t()
+        edge_h = torch.cat([h[edge[0, :], :], h[edge[1, :], :]], dim=1).t()
 
         edge_e = torch.exp(-self.leakyrelu(self.a.mm(edge_h).squeeze()))
         assert not torch.isnan(edge_e).any()
